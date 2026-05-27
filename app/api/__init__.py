e91adda5cb84e0c742966a28b8c410ac96347988
Rishabh Das <rishabh.babi@gmail.com>
2026-05-27 18:06:05 +0530

    .

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,5 @@
 """Transport module."""
 
-from .mcp_sse import router as mcp_sse_router
 from .agent_routes import router as agent_routes_router
 
-__all__ = ["mcp_sse_router", "agent_routes_router"]
+__all__ = ["agent_routes_router"]
