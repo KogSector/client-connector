8771c19293de15932d8b5f8db6da1fe62384e2c1
Rishabh Das <rishabh.babi@gmail.com>
2026-06-30 19:24:34 +0530

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 """App package."""
 
 from .config import Settings, get_settings
-from .main import app, create_app
 
-__all__ = ["Settings", "app", "create_app", "get_settings"]
+__all__ = ["Settings", "get_settings"]
